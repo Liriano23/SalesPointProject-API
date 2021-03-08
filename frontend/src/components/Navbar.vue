bad2b4ab10aa14b5676b96451f3b653a20ccf3f3
Natanael <NatanaelUF17@gmail.com>
2021-03-07 20:43:44 -0400

    Adding routing and basic proyect template

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+    </div>
+
+    <div class="d-flex align-center">
+      <v-btn to="/" depressed color="primary">
+        Home
+      </v-btn>
+      <v-btn to="/categories" depressed color="primary">
+        Categories
+      </v-btn>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style></style>
---
+++ frontend/src/views/Categories.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>This is the categories view</h1>
+</template>
+
+<script>
+export default {
+    name: 'Categories'
+}
+</script>
+
+<style>
+
+</style>
